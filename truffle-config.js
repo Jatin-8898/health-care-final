b2f4a5918b50fe8cd16862d8e7fdf89563b89f10
Jatin Varlyani <varlyanijatin88@gmail.com>
2020-04-11 15:03:41 +0530

    Final changes done

--- truffle-config.js
+++ truffle-config.js
@@ -44,12 +44,12 @@ module.exports = {
     //
     development: {
         host: "127.0.0.1",     // Localhost (default: none)
-        port: 8545,            // Standard Ethereum port (default: none)
+        port: 7545,            // Standard Ethereum port (default: none)
         network_id: "*",       // Any network (default: none) 
     },
     test:{
         host:"127.0.0.1",
-        port:9545,
+        port:7545,
         network_id:"*"
       }/* ,
     ganache:{
